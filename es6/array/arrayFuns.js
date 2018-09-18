37c707aae62a753f34077e98b1df23c979fcf9a2
张锋 <zhangfeng930101@qq.com>
2018-09-18 14:43:24 +0800

    array from

--- es6/array/arrayFuns.js
+++ es6/array/arrayFuns.js
@@ -1,5 +1,17 @@
 class ArrayFun {
   constructor() {
-    console.log(this)
+    // console.log(this)
+  }
+  // 删除数组末尾的元素 返回值为被删除的元素
+  arrPop(arr) {
+    return arr.pop()
+  }
+  // 末尾添加元素
+  arrPush(arr, item) {
+    // console.log(arr.push(item))
+    return arr.push(item)
+  }
+  arrFrom(item) {
+    return Array.from(item)
   }
 }
