f29cdf019abc305d3990ca08f15d3d216215ffdf
张锋 <zhangfeng930101@qq.com>
2018-10-22 20:40:46 +0800

    loader

--- webpack/src/index.js
+++ webpack/src/index.js
@@ -1,10 +1,19 @@
 import _ from 'lodash'
+import './style.css'
+import loginIcon from './image/login.jpg'
+import data from './data.xml'
 
 function component() {
   var element = document.createElement('div')
 
   element.innerHTML = _.join(['Hello', 'webpack'], '~')
+  element.classList.add('hello')
 
+  let myHeader = new Image()
+  myHeader.src = loginIcon
+  myHeader.width = '100'
+  element.appendChild(myHeader)
+  console.log(data)
   return element
 }
 
