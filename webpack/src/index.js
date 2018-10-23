6cf17440e02e9db09e585148717b5d4579857df3
张锋 <zhangfeng930101@qq.com>
2018-10-23 19:41:31 +0800

    s

--- webpack/src/index.js
+++ webpack/src/index.js
@@ -1,19 +1,24 @@
 import _ from 'lodash'
-import './style.css'
-import loginIcon from './image/login.jpg'
-import data from './data.xml'
+// import './style.css'
+// import loginIcon from './image/login.jpg'
+// import data from './data.xml'
+import printMe from './print'
 
 function component() {
   var element = document.createElement('div')
+  var btn = document.createElement('button')
 
   element.innerHTML = _.join(['Hello', 'webpack'], '~')
   element.classList.add('hello')
 
-  let myHeader = new Image()
-  myHeader.src = loginIcon
-  myHeader.width = '100'
-  element.appendChild(myHeader)
-  console.log(data)
+  btn.innerHTML = '点击我print'
+  btn.onclick = printMe
+  element.appendChild(btn)
+
+  // let myHeader = new Image()
+  // myHeader.src = loginIcon
+  // myHeader.width = '100'
+  // element.appendChild(myHeader)
   return element
 }
 
