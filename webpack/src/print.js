7ea35340ca8c59e448ffdb78034ff552d646435c
张锋 <zhangfeng930101@qq.com>
2018-10-31 08:59:14 +0800

    common

--- webpack/src/print.js
+++ webpack/src/print.js
@@ -1,3 +1,14 @@
+import { cube } from './main';
+
 export default function printMe () {
   console.log("webpack")
 }
+function component() {
+  var element = document.createElement('div')
+  
+  element.innerHTML = _.join(['Hello webpack', '5 cubed is equal to' + cube(5)], '~')
+
+  return element
+}
+
+document.body.appendChild(component())
--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -1,10 +1,12 @@
 const path = require('path');
+const webpack = require('webpack')
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: {
-    app: './src/index.js'
+    app: './src/index.js',
+    another: './src/print.js'
   },
   output: {
     filename: '[name].bundle.js',
@@ -12,8 +14,37 @@ module.exports = {
   },
   plugins: [
     new CleanWebpackPlugin(['dist']),
+    // new HtmlWebpackPlugin({
+    //   title: 'Code Spliting'
+    // }),
+
     new HtmlWebpackPlugin({
-      title: 'production'
-    })
-  ]
+      title: 'index',
+      filename: 'index.html',
+      template: './src/index.html',
+      chunks: ['app', 'vendor']
+    }),
+    new HtmlWebpackPlugin({
+      title: 'second',
+      filename: 'second.html',
+      template: './src/second-index.html',
+      chunks: ['another']
+    }),
+    // 提取公共的js文件 webpack4.0以上 换做和entry同级别的插件 splitChunksPlugin
+    // new webpack.optimize.CommonsChunkPlugin({
+    //   name: 'common'
+    // })
+  ],
+  optimization: {
+    splitChunks: {
+      chunks: 'initial',
+      cacheGroups: {
+        vendor: {
+          name: 'vendor',
+          chunks: "all",
+          minChunks: 2
+        }
+      }
+    }
+  }
 }
